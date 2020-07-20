5b2d472283f9abd51d2e9e8a92e81d78b630e566
vijaykanth1729 <personalvijay007@gmail.com>
2020-07-20 09:36:47 +0530

    updated hello file

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hey hello dude!! updated again now..")
+data = ["apple", "banana", "oranges"]
+for i in data:
+    print(i)
+
+n = 3
+for i in range(n):
+    for j in range(i+1):
+        print(chr(65+j), end=" ")
+    print()
+
